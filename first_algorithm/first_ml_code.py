2d7b55c58f11a81c58241d5053734e1466768d8d
Persifer <giorgino.antonio32@protonmail.com>
2021-08-28 15:43:03 +0200

    Import of the first dataset

--- first_algorithm/first_ml_code.py
+++ first_algorithm/first_ml_code.py
@@ -1,1 +1,7 @@
-print("Hello world!")
+#preloaded dataset that contains information about terrains and prices in Boston
+from sklearn.datasets import load_boston
+
+#load the Boston's dataset into the var dataset
+dataset = load_boston()
+
+print(dataset['DESCR'])
