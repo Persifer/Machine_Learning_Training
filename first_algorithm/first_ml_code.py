f3db3c53f71e0ecfb43377818fa41b5b7a0d3023
Persifer <giorgino.antonio32@protonmail.com>
2021-08-29 12:53:47 +0200

    Minor changes and creation of .gitignore

--- first_algorithm/first_ml_code.py
+++ first_algorithm/first_ml_code.py
@@ -1,11 +1,12 @@
 #preloaded dataset that contains information about terrains and prices in Boston
 from sklearn.datasets import load_boston
 
-
-
 def printDescription(dataset):
     #printing a description of the dataset
     print(dataset['DESCR'])
+
+    for data in dataset['data']:
+        print(data)
 
 if __name__ == '__main__':
     # load the Boston's dataset into the var dataset
