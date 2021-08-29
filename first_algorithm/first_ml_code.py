03e85ade5d6917bd1d11cbb1f83ddad3520e0c65
Persifer <giorgino.antonio32@protonmail.com>
2021-08-29 12:50:27 +0200

    Minor changes

--- first_algorithm/first_ml_code.py
+++ first_algorithm/first_ml_code.py
@@ -1,12 +1,13 @@
 #preloaded dataset that contains information about terrains and prices in Boston
 from sklearn.datasets import load_boston
 
-#load the Boston's dataset into the var dataset
-dataset = load_boston()
 
-def printDescription():
+
+def printDescription(dataset):
     #printing a description of the dataset
     print(dataset['DESCR'])
 
 if __name__ == '__main__':
-    printDescription()
+    # load the Boston's dataset into the var dataset
+    dataset = load_boston()
+    printDescription(dataset)
