c02186bd5307bff8ef7603dd47c8d6a9abdb69d3
Persifer <giorgino.antonio32@protonmail.com>
2021-09-04 13:10:44 +0200

    Importing and using the iris dataset

--- classification_algorithm/classification_algorithm.py
+++ classification_algorithm/classification_algorithm.py
@@ -1,3 +1,9 @@
+from sklearn.datasets import load_iris
+
+def training_classificator(dataset):
+    print("Hi!")
 
 if __name__ == '__main__':
-    print("Hello World")
+    dataset = load_iris()
+
+    training_classificator(dataset)
