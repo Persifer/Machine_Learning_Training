b388462afd3537273621588a9ce874a2c474c970
Persifer <giorgino.antonio32@protonmail.com>
2021-09-04 13:39:39 +0200

    Splitting the data into data for training and data for the test

--- classification_algorithm/classification_algorithm.py
+++ classification_algorithm/classification_algorithm.py
@@ -1,7 +1,17 @@
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
 
 def training_classificator(dataset):
-    print("Hi!")
+    # taking the data of the iris flower from the dataset
+    X = dataset['data']
+    # taking the classification of the flower from the dataset
+    # 0 = Setosa | 1 = Versicolurs | 2 = Virginica
+    y =  dataset['target']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+
+
 
 if __name__ == '__main__':
     dataset = load_iris()
